ce53f383ec36502b830b3c15f6d9e868001cec7a
Jeff Swisher <jefftswisher@gmail.com>
2020-01-30 00:07:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,11 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles/#{ @article.id }"
   end 
   
-  
+  delete '/articles/:id' do 
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+  end 
   
   
   
